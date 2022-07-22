18f996bee14f5a06cb39a88884e6a6c594fd55b7
陈泽宣 <qchenzexuan@vip.qq.com>
2022-07-22 23:19:18 +0800

    修改面包屑配置,修改个人信息下拉菜单内容

--- src/views/404.vue
+++ src/views/404.vue
@@ -10,7 +10,7 @@
       <div class="bullshit">
         <div class="bullshit__oops">OOPS!</div>
         <div class="bullshit__info">版权所有
-          <a style="color:#20a0ff" href="https://wallstreetcn.com" target="_blank">合智源</a>
+          <a style="color:#20a0ff" href="https://izhangxr.cn" target="_blank">合智源</a>
         </div>
         <div class="bullshit__headline">{{ message }}</div>
         <div class="bullshit__info">请检查您输入的网址是否正确，或点击下方按钮返回首页。</div>
